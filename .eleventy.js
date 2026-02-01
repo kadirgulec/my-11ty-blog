4d8e83ac5b8d5240ec154c72290c12666a99cca4
kg <devguelec@gmail.com>
2026-02-01 13:08:54 +0100

    Adds last updated shortcode and typography plugin
    Adds a shortcode to display the last updated date in the footer.
    
    Installs and configures the `@tailwindcss/typography` plugin for improved content styling.
    
    Removes a duplicated README.md file.

--- .eleventy.js
+++ .eleventy.js
@@ -10,6 +10,18 @@ module.exports = function(eleventyConfig) {
         }).replace(',', '');
     });
 
+    eleventyConfig.addShortcode("lastUpdated", () => {
+        return new Date().toLocaleDateString('de-DE', {
+            timeZone: 'Europe/Berlin',
+            year: 'numeric',
+            month: '2-digit',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false
+        });
+    })
+
     eleventyConfig.addGlobalData("year", () => new Date().getFullYear());
     return {
         dir: {
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # my-11ty-blog
 - npm install
 - npm start
+
+# Ideas
+- RSS feed
+- Tags Featured for the blog posts to show home page
+- projects to the live projects like ContratMonitor
+- Contact page
